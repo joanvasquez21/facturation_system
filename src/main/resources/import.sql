789bed4cf820af47d16407eb7728fb5999a054f3
joanvasquez21 <joanvrivas.21@gmail.com>
2024-09-20 20:04:09 +0200

    feat: the list of invoices is added to the clients view page

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,3 +6,13 @@ INSERT INTO clients(name, lastname, email, create_at, photo) values ('Joan', 'Va
 INSERT INTO clients(name, lastname, email, create_at, photo) values ('Joan', 'Vasquez', 'j@g.com', '2023-03-03', '');
 INSERT INTO clients(name, lastname, email, create_at, photo) values ('Joan', 'Vasquez', 'j@g.com', '2023-03-03', '');
 INSERT INTO clients(name, lastname, email, create_at, photo) values ('Joan', 'Vasquez', 'j@g.com', '2023-03-03', '');
+
+INSERT INTO products (name, price, create_at) VALUES ('Tv',  23244, now());
+INSERT INTO products (name, price, create_at) VALUES ('Tv',  23244, now());
+INSERT INTO products (name, price, create_at) VALUES ('Tv',  23244, now());
+INSERT INTO products (name, price, create_at) VALUES ('Tv',  23244, now());
+INSERT INTO products (name, price, create_at) VALUES ('Tv',  23244, now());
+INSERT INTO products (name, price, create_at) VALUES ('Tv',  23244, now());
+INSERT INTO products (name, price, create_at) VALUES ('Tv',  23244, now());
+INSERT INTO products (name, price, create_at) VALUES ('Tv',  23244, now());
+
